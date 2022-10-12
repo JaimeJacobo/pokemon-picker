c04d6636392fc5291a1ff418c798e274658ca8ca
Jaime Jacobo <jaimejacobo@Jaimes-Mac-mini.local>
2022-10-12 13:14:13 +0900

    adding functionllity to the list

--- src/components/PokemonPicker.jsx
+++ src/components/PokemonPicker.jsx
@@ -1,29 +1,59 @@
+import pokemonList from './data/pokemonList.json'
+
+import { useState, useEffect } from 'react'
 import { useFetchPokemons } from '../hooks/useFetchPokemons'
 
 const PokemonPicker = () => {
+  const [versions, setVersions] = useState([])
+  const [selectedVersions, setSelectedVersions] = useState([])
+
+  useEffect(() => {
+    const versions = []
+    for (let i = 1; i <= pokemonList.totalGenerations; i++) {
+      versions.push({ version: i, show: true })
+    }
+    setVersions(versions)
+  }, [])
+
   const pokemons = useFetchPokemons()
+
+  const handleInputChange = (e) => {
+    setSelectedVersions((prev) => {
+      return [...prev, e.target.value]
+    })
+  }
+
+  const renderPokemons = () => {
+    return pokemons?.map((pokemon) => {
+      return (
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          key={pokemon.id}
+        ></img>
+      )
+    })
+  }
+
   return (
     <div className="PokemonPicker">
-      <input
-        type="checkbox"
-        name="vehicle1"
-        value="Bike"
-        // onChange={(e) => console.log(e.target.checked)}
-      />
-      <label htmlFor="vehicle1"> Generation 1</label>
-      <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
-      <label htmlFor="vehicle2"> Generation 2</label>
-      <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
-      <label htmlFor="vehicle3"> Generation 3</label>
-      {pokemons?.map((pokemon) => {
+      {versions?.map((generation) => {
         return (
-          <img
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            key={pokemon.id}
-          ></img>
+          <div key={generation.version}>
+            <input
+              id={generation.version}
+              type="checkbox"
+              value={generation.version}
+              defaultChecked={true}
+              onChange={(e)=>handleInputChange(e)}
+            />
+            <label htmlFor={generation.version}>
+              Generation {generation.version}
+            </label>
+          </div>
         )
       })}
+      {renderPokemons()}
     </div>
   )
 }
